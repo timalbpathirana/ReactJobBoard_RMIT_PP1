bec7f0fdef07841288ca65aebaf1c1185bbb5b2a
Timal Pathirana <timalbathiya1995@gmail.com>
2021-04-22 01:41:49 +1000

    added database codes and fetching from firebase

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Box, Grid, ThemeProvider } from "@material-ui/core";
 import theme from "./theme/theme";
 import Header from "./Components/Header";
@@ -7,8 +7,29 @@ import JobCard from "./Components/Job/JobCard";
 import NewJobModal from "./Components/Job/NewJobModal";
 import jobData from "./dummyData";
 import Login from "./Components/login.js/Login";
+import { firestore, app } from "./firebase/config";
 
 export default () => {
+  // setting the state
+  const [jobs, setJobs] = useState([]);
+  // fetching the jobs from database when user loads the website.
+  const fetchJobs = async () => {
+    const req = await firestore
+      // collection name
+      .collection("jobs")
+      // order by posted on time and decending order
+      .orderBy("postedOn", "desc")
+      // fetching the data
+      .get();
+
+    // looping through the database and pull the infomation to the variable job and also ising ... spread function and overiding the array with data + id
+    //saving that to the tempjob
+    const tempJob = req.docs.map((job) => ({ ...job.data(), id: job.id }));
+    setJobs(tempJob);
+  };
+  useEffect(() => {
+    fetchJobs();
+  }, []);
   return (
     <ThemeProvider theme={theme}>
       <Header />
@@ -16,7 +37,8 @@ export default () => {
       <Grid container justify="center">
         <Grid item xs={10}>
           <SearchBar />
-          {jobData.map((job) => (
+          {/* fetching from jobs */}
+          {jobs.map((job) => (
             <JobCard key={job.id} {...job} />
           ))}
         </Grid>
--- src/database/create_db.js
+++ src/database/create_db.js
@@ -1,16 +1,16 @@
-var mysql = require('mysql');
+// var mysql = require('mysql');
 
-var con = mysql.createConnection ({
-    host:"localhost",
-    user:"yourusername",
-    password:"youruserpassword";
-});
+// var con = mysql.createConnection ({
+//     host:"localhost",
+//     user:"yourusername",
+//     password:"youruserpassword";
+// });
 
-con.connect(function(err)){
-    if(err)throw err;
-    console.log("Connected!");
-    con.query("CREATE DATABASE db",function(err result)){
-        if(err) throw err;
-        console.log("Database created");
-    });
-});
+// con.connect(function(err)){
+//     if(err)throw err;
+//     console.log("Connected!");
+//     con.query("CREATE DATABASE db",function(err result)){
+//         if(err) throw err;
+//         console.log("Database created");
+//     });
+// });
